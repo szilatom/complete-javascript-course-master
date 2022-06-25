5d0383ecc64ac05870a2313daf796f201e82352b
Thomas Design <szilatom@gmail.com>
2022-06-25 10:55:36 +0100

    #1 Project: change style when player win

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
 // for testing
 document.querySelector('.number').textContent = secretNumber;
 //
+
 // Set score
 let score = 20;
 
@@ -14,36 +16,37 @@ function classTextMod(className, text) {
 function checkBtnClick() {
   const guess = Number(document.querySelector('.guess').value);
 
+  // When there is no number
   if (!guess) {
-    //document.querySelector('.message').textContent = 'No Number';
     classTextMod('message', 'No Number');
+
+    // When the player win
   } else if (guess === secretNumber) {
-    //document.querySelector('.message').textContent = 'Correct number';
     classTextMod('message', 'Correct number');
+    // change body CSS style
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    // change number color
+    document.querySelector('.number').style.color = 'green';
+
+    // When the guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
-      //document.querySelector('.message').textContent = 'Too high';
       classTextMod('message', 'Too high');
       score--;
-      //document.querySelector('.score').textContent = score;
       classTextMod('score', score);
     } else {
-      //document.querySelector('.message').textContent = 'You lost the game';
       classTextMod('message', 'You lost the game');
-      //document.querySelector('.score').textContent = 0;
       classTextMod('score', 0);
     }
+
+    // When the guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
-      //document.querySelector('.message').textContent = 'Too low';
       classTextMod('message', 'Too low');
       score--;
-      //document.querySelector('.score').textContent = score;
       classTextMod('score', score);
     } else {
-      //document.querySelector('.message').textContent = 'You lost the game';
       classTextMod('message', 'You lost the game');
-      //document.querySelector('.score').textContent = 0;
       classTextMod('score', 0);
     }
   }
