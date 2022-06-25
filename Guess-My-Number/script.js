9d4dc8c571a2b15707063d7fe65822c3db2de0c3
Thomas Design <szilatom@gmail.com>
2022-06-25 06:49:46 +0100

    #1 Project: generate random number, count the scores

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -9,12 +9,39 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+//console.log(number);
+// for testing
+document.querySelector('.number').textContent = secretNumber;
+//
+// Set score
+let score = 20;
 
 function checkBtnClick() {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 }
 
