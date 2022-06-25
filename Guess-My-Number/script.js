eb1fa8eb16efa07e85269ea25dc221dc4f743d6c
Thomas Design <szilatom@gmail.com>
2022-06-25 07:28:56 +0100

    #1 Project: add classTextMod function

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,46 +1,50 @@
 'use strict';
-/*
-document.querySelector('.message').textContent = 'Correct number';
-console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-//console.log(number);
 // for testing
 document.querySelector('.number').textContent = secretNumber;
 //
 // Set score
 let score = 20;
 
+function classTextMod(className, text) {
+  document.querySelector(`.${className}`).textContent = text;
+}
+
 function checkBtnClick() {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number';
+    //document.querySelector('.message').textContent = 'No Number';
+    classTextMod('message', 'No Number');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct number';
+    //document.querySelector('.message').textContent = 'Correct number';
+    classTextMod('message', 'Correct number');
   } else if (guess > secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high';
+      //document.querySelector('.message').textContent = 'Too high';
+      classTextMod('message', 'Too high');
       score--;
-      document.querySelector('.score').textContent = score;
+      //document.querySelector('.score').textContent = score;
+      classTextMod('score', score);
     } else {
-      document.querySelector('.message').textContent = 'You lost the game';
-      document.querySelector('.score').textContent = 0;
+      //document.querySelector('.message').textContent = 'You lost the game';
+      classTextMod('message', 'You lost the game');
+      //document.querySelector('.score').textContent = 0;
+      classTextMod('score', 0);
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low';
+      //document.querySelector('.message').textContent = 'Too low';
+      classTextMod('message', 'Too low');
       score--;
-      document.querySelector('.score').textContent = score;
+      //document.querySelector('.score').textContent = score;
+      classTextMod('score', score);
     } else {
-      document.querySelector('.message').textContent = 'You lost the game';
-      document.querySelector('.score').textContent = 0;
+      //document.querySelector('.message').textContent = 'You lost the game';
+      classTextMod('message', 'You lost the game');
+      //document.querySelector('.score').textContent = 0;
+      classTextMod('score', 0);
     }
   }
 }
