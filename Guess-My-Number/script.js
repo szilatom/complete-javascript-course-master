2f2c25303f533b9d04a76169913f9e47411cb2d0
Thomas Design <szilatom@gmail.com>
2022-06-25 06:07:18 +0100

    #1 Project: add click event

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,10 +1,21 @@
 'use strict';
-
-console.log(document.querySelector('.message').textContent);
+/*
 document.querySelector('.message').textContent = 'Correct number';
+console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+function checkBtnClick() {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+  }
+}
+
+document.querySelector('.check').addEventListener('click', checkBtnClick);
